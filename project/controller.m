b8cf1a8fc897c6250ccf3c58a654ee8809e267b5
Ray Zhang <rayzhang1997@yahoo.com>
2021-05-08 18:06:15 -0400

    upload report with traj_gen and controller

--- project/controller.m
+++ project/controller.m
@@ -7,6 +7,9 @@ cf = crazyflie();
 %
 Kd = [10;10;10];
 Kp = [10;10;10];
+Kd_2 = [10;10;10];
+Kp_2 = [10;10;10];
+
 
 % x = state(1:3);
 % v = state(4:6);
@@ -21,24 +24,25 @@ yaw_dt = current_state{n}.yawdot;
 angle = current_state{n}.angle;
 omega = current_state{n}.omega;
 
-x_des = desired_state{n}.pos;
-x_dt_des = desired_state{n}.vel;
-x_ddt_des = desired_state{n}.acc;
-yaw_des = desired_state{n}.yaw;
-yaw_dt_des = desired_state{n}.yawdot;
+% desired states from trajectory generator 
+x_T = desired_state{n}.pos;
+x_dt_T = desired_state{n}.vel;
+x_ddt_T = desired_state{n}.acc;
+yaw_T = desired_state{n}.yaw;
+yaw_dt_T = desired_state{n}.yawdot;
 
 %desired acceleration 
-x_ddt_des = x_ddt_des - Kd.*(x_dt - x_dt_des) - Kp.*(x - x_des);
+x_ddt_des = x_ddt_T - Kd.*(x_dt - x_dt_T) - Kp.*(x - x_T);
 
-% Desired roll, pitch
-phi_des = 1/cf.g * (x_ddt_des(1)*sin(yaw_des) - x_ddt_des(2)*cos(yaw_des));
-theta_des = 1/cf.g * (x_ddt_des(1)*cos(yaw_des) + x_ddt_des(2)*sin(yaw_des));
-psi_des = yaw_des;
+% Desired roll, pitch, yaw
+phi_des = 1/cf.g * (x_ddt_T(1)*sin(yaw_T) - x_ddt_T(2)*cos(yaw_T));
+theta_des = 1/cf.g * (x_ddt_T(1)*cos(yaw_T) + x_ddt_T(2)*sin(yaw_T));
+psi_des = yaw_T;
 
 angle_des = [phi_des;theta_des;psi_des];
-omega_des = [0;0;yaw_dt_des];
+omega_des = [0;0;yaw_dt_T];
 
 
 u1 = cf.mass * x_ddt_des(3,1) + cf.mass * cf.g;
 
-u2 = cf.I * (-Kd_2 * (omega - omega_des) - Kp_2 * (angle - angle_des);
+u2 = cf.I * (-Kd_2 * (omega - omega_des) - Kp_2 * (angle - angle_des));
